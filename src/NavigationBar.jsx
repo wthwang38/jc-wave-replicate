0c698d9815c3a925aa7c15699e3176689923c9b4
William Hwang <willhwang90@gmail.com>
2023-09-25 17:46:53 -0400

    fix sides of the navbar, change how the sign in and sign up button looks

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -9,10 +9,10 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 const NavigationBar = () => {
   return (
     <Navbar expand='lg' className="bg-body-tertiary" sticky="top">
+      <Container fluid>
         <Navbar.Brand href='/'><img alt='logo' src='/vite.svg' width='30' height='30' className='d-inline-block align-top'/>BRAND NAME</Navbar.Brand>
         <Navbar.Toggle/>
         <Navbar.Collapse className="justify-content-end">
-        <Container>
         <Nav className="justify-content-end">
             <NavDropdown title='Features' id='basic-nav-dropdown'>
               <NavDropdown.Item href='/rarity'>Rarity</NavDropdown.Item>
@@ -25,11 +25,11 @@ const NavigationBar = () => {
               <NavDropdown.Item href='/helpcenter'>Help Center</NavDropdown.Item>
               <NavDropdown.Item href='/sitemap'>Site Map</NavDropdown.Item>
             </NavDropdown>
-            <Button variant='outline-light' size='sm' style={{ borderRadius: '2rem', color:'black', borderColor:'black'}} href='/signin'> Sign in </Button>
-            <Button size='sm' style={{ borderRadius: '2rem' }} href='signup'> Sign up! </Button>
+            <Button variant='outline-secondary' size='md'  href='/signin'> Sign in </Button>
+            <Button size='md' href='signup'> Sign up! </Button>
           </Nav>
-        </Container>
         </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
